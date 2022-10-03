fb10e4ddd941ab740774c6a085e3e2c378991b5b
DavidGRueda <glezdavid00@gmail.com>
2022-10-03 13:13:04 +0200

    Create new phones

--- phoneer/src/services/getPhones.js
+++ phoneer/src/services/getPhones.js
@@ -1,4 +1,5 @@
 export default function getPhones() {
-  const path = process.env.API_PATH;
-  return fetch(`http://${path}}/getPhones`).then((res) => res.json());
+  //const path = process.env.REACT_APP_API_PATH;
+  const path = "localhost:3001";
+  return fetch(`http://${path}/getPhones`).then((res) => res.json());
 }
